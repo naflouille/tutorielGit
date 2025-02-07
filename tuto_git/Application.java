fc68bf1f969d2a30f252ddd9bde784edd9bf9ad8
Carla_DAUBAN <carladauban08@gmail.com>
2025-02-07 09:36:18 +0100

    ajout d'un titre à la fenêtre

--- tuto_git/Application.java
+++ tuto_git/Application.java
@@ -17,7 +17,7 @@ public class Application extends Frame {
 		Vue vue = new Vue();
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
-		this.setTitle("tutoriel git");
+		this.setTitle("daveLeNafouilleur");
 		this.add(vue);
 
 		this.setLocationRelativeTo(null);
